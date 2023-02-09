e883cc03aa9bb5842b9f1bb3e9628764643e0de5
Abel Yitages Demissie <99829666+kiya3300@users.noreply.github.com>
2023-02-09 21:19:18 +0300

    Alx-Interview: Starwars-Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,17 @@
 #!/usr/bin/node
-
 const request = require('request');
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    recursionRequestPrint(characters, 0);
+  }
+});
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
+function recursionRequestPrint (url, index) {
+  request(url[index], (err, res, body) => {
+    if (!err) {
       console.log(JSON.parse(body).name);
-      req(arr, i + 1);
+      if (index + 1 < url.length) recursionRequestPrint(url, ++index);
     }
   });
-};
-
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
-  }
-);
+}
